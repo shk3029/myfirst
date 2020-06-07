aa9b7be15aedbf6b49cfdee4320dff42c2566cbb
shk3029 <shk30290@naver,com>
2020-06-07 10:28:46 +0900

    자동완성, 검색 커밋

--- lib/dio_server.dart
+++ lib/dio_server.dart
@@ -1,19 +1,32 @@
 import 'package:dio/dio.dart';
-import 'dart:convert';
 
-const _Cookle_Prefix = "http://localhost:8080/auto/cook?query=ㅇ";
+const _Cookle_Auto_Prefix = "http://localhost:8080/auto/cook?query=ㅇ";
+const _Cookle_Search_Prefix = "http://localhost:8080/search/cook?query=양파,소금";
 
 class Server {
- Future<void> getReq() async {
-   Response<Map> response;
-   Dio dio = new Dio();
-   response = await dio.get("$_Cookle_Prefix");
-   Map responseBody = response.data;
-   List<dynamic> list = responseBody["data"]["esautocook"];
-   list.forEach((element) {
-     print(element["keyword"]);
-   });
- }
+
+  Future<void> getReq() async {
+    Response<Map> response;
+    Dio dio = new Dio();
+    response = await dio.get("$_Cookle_Auto_Prefix");
+    Map responseBody = response.data;
+    List<dynamic> list = responseBody["data"]["esautocook"];
+    list.forEach((element) {
+      print(element["keyword"]);
+    });
+  }
+
+  Future<void> getSearchReq() async {
+    Response<Map> response;
+    Dio dio = new Dio();
+    response = await dio.get("$_Cookle_Search_Prefix");
+    Map responseBody = response.data;
+    List<dynamic> list = responseBody["data"]["essearchcook"];
+    list.forEach((element) {
+      print(element["title"]);
+      print(element["cookTime"]);
+    });
+  }
 }
 
 Server server = Server();
